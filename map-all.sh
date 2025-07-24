453f44f215ac49b5c58c9f1c3f4cef9a9b38690e
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-07-24 18:26:28 +0200

    Fix issue with string escaping in jq output

--- map-all.sh
+++ map-all.sh
@@ -18,15 +18,20 @@ for MAPPING in $MAPPING_DIR/*.jq; do
         echo "Mapping file '$INPUT'..."
         INPUT_NAME=`basename $INPUT .jsonl`
         mkdir -p "$JSONLD_OUTPUT_DIR/$TYPE"
-        OUTPUT_PREFIX="$JSONLD_OUTPUT_DIR/$TYPE/$INPUT_NAME"
+        JSONLD_OUTPUT_PREFIX="$JSONLD_OUTPUT_DIR/$TYPE/$INPUT_NAME"
+        # JSONLD_LINE_OUTPUT_PREFIX="$JSONLD_OUTPUT_DIR/$TYPE/${INPUT_NAME}_line"
+        # JSONLDL_OUTPUT="$JSONLD_OUTPUT_DIR/$TYPE/${INPUT_NAME}.jsonldl"
 #        OUTPUT_PREFIX=$OUTPUT_DIR/$TYPE-$ITER.jsonld
         # jq -f $MAPPING $INPUT --compact-output | awk '{ system("echo '\''"$0"'\'' >'$OUTPUT_PREFIX'_" NR ".jsonld") }'
         # jq -f $MAPPING $INPUT --compact-output | xargs -0 -n1 echo >"${OUTPUT_PREFIX}.jsonld"
-        ITER=0
+        ITER=1
         # jq -f $MAPPING $INPUT --compact-output | while read out; do 
-        jq -L $MAPPING_DIR 'include "'$TYPE'"; main | ."@context" = '$JSONLD_CONTEXT'' $INPUT --compact-output | while read out; do 
-            echo "$out" | jq "." >"${OUTPUT_PREFIX}_${ITER}.jsonld"
-            echo "$out" | jsonld toRdf - --n-quads >>$NQ_OUTPUT
+        # jq -L $MAPPING_DIR 'include "'$TYPE'"; main | ."@context" = '$JSONLD_CONTEXT'' $INPUT --compact-output >${JSONLDL_OUTPUT}
+        jq -L $MAPPING_DIR 'include "'$TYPE'"; main | ."@context" = '$JSONLD_CONTEXT'' $INPUT --compact-output | while read -r out; do 
+            echo "Mapping JSON n. '$ITER'..."
+            # printf '%s' "$out" >"${JSONLD_LINE_OUTPUT_PREFIX}_${ITER}.jsonld"
+            jq "." <<<"$out" >"${JSONLD_OUTPUT_PREFIX}_${ITER}.jsonld"
+            jsonld toRdf - --n-quads  <<<"$out" >>$NQ_OUTPUT
             ITER=$(expr $ITER "+" 1)
         done
 #        ITER=$(expr $ITER "+" 1)
